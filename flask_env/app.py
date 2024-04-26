8e4de0cb9243ce8f761e1b14354d54a6368b5a6c
Howard <57738845+Chaoward@users.noreply.github.com>
2024-04-26 00:34:31 -0700

    Update app.py

--- flask_env/app.py
+++ flask_env/app.py
@@ -9,7 +9,8 @@ import config
 app = Flask(__name__)
 CORS(app)
 
-LAST_MODID_REQUEST = None;  
+LAST_MODID_REQUEST = None
+IS_TRAINING = False  
 
 def isAllowedFile(fileName):
    ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "gif"}
